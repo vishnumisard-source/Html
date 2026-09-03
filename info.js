9f7c1b6d2a074a420c965b7155b80190e63fd6ed
vishnumisard-source <vishnumisard@gmail.com>
2026-09-03 11:12:15 +0530

    Add files via upload

---
+++ info.js
@@ -0,0 +1,1 @@
+export var companyname = "misard";
---
+++ math.js
@@ -0,0 +1,16 @@
+//module example
+
+//call the add and subtract to app.js how first export and import
+export function add(a,b){
+    return a + b;
+}
+
+export function subtract(a,b){
+    return a - b;
+}
+
+export default function getcompanylog(){
+            return "Misard";
+}
+
+//multiple default cant use in this 
